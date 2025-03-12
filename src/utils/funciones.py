17beeccb2d00f150ed8aba01088aad670cb680e2
JoseRKC <josercc@gmail.com>
2025-03-12 20:52:00 +0100

    actualización 12/3

---
+++ src/utils/funciones.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+month_to_day = {
+    "jan": 1, "feb": 2, "mar": 3, "apr": 4, "may": 5, "jun": 6,
+    "jul": 7, "aug": 8, "sep": 9, "oct": 10, "nov": 11, "dec": 12
+}
+
+# Función para convertir el mes de texto a número
+
+def convert_month_to_day(X):
+    return np.array([[month_to_day[m]] for m in X.values.ravel()])
